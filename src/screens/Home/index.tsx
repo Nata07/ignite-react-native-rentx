459b815089e370740c25ad3969d7bc14b16566db
Natanael <nata.mw@hotmail.com>
2021-06-23 01:07:19 -0300

    :tada: initial commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container, Title } from './styles';
+
+export const Home: React.FC = () => {
+  return (
+    <Container>
+      <Title>App List Service Home</Title>
+    </Container>
+  );
+}
