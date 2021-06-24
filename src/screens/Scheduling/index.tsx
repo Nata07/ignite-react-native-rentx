17e2ec6d9cc12626ffb2058e9cd89d9a4d883d28
Natanael <nata.mw@hotmail.com>
2021-06-23 23:25:38 -0300

    :sparkles: feat: created compoenent Scheduling

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { StatusBar, View } from 'react-native';
+import { BackButton } from '../../components/BackButton';
+import theme from '../../styles/theme';
+import { Button } from '../../components/Button';
+
+import { 
+  Container, 
+  Header, 
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer
+ } from './styles';
+
+import ArrowSvg from '../../assets/arrow.svg'
+
+export function Scheduling() {
+  return (
+    <Container>
+      <StatusBar barStyle="light-content" />
+      <Header>
+        <BackButton color={theme.colors.shape}  />
+
+        <Title>
+          Escolha uma {'\n'}
+          data de inicio e {'\n'}
+          fim do aluguel 
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={true}>18/06/2021</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={false}>18/06/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content>
+
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" color={theme.colors.main} onClick={() => {}}/>
+      </Footer>
+    </Container>
+  );
+}
