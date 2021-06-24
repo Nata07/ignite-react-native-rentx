03f85a5ab3e4c4183ccd0c18bf01b3ea331a0350
Natanael <nata.mw@hotmail.com>
2021-06-23 23:17:00 -0300

    :sparkles: feat: listed cars in home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,66 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
-import { Container, Title } from './styles';
+import { Container, Header, HeaderContent, TotalCars, TestCar } from './styles';
+
+import Logo from '../../assets/logo.svg';
+
+import { RFValue } from 'react-native-responsive-fontsize';
+import {Car} from '../../components/Car';
+import { ScrollView } from 'react-native';
 
 export const Home: React.FC = () => {
+  const data = [{
+    name: 'RSç 5 Coupé',
+    brand: 'Audi',
+    period: 'Ao dia',
+    price: '125,00',
+    image_url: 'https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png'
+  },
+  {
+    name: 'PANAMERA',
+    brand: 'PORSHE',
+    period: 'Ao dia',
+    price: '325,00',
+    image_url: 'https://catalogo.webmotors.com.br/imagens/prod/347468/PORSCHE_PANAMERA_2.9_V6_EHYBRID_4_PDK_34746814472691347.png?s=fill&w=440&h=330&q=80&t=true'
+  },
+  {
+    name: 'PANAMERA',
+    brand: 'PORSHE',
+    period: 'Ao dia',
+    price: '325,00',
+    image_url: 'https://catalogo.webmotors.com.br/imagens/prod/347468/PORSCHE_PANAMERA_2.9_V6_EHYBRID_4_PDK_34746814472691347.png?s=fill&w=440&h=330&q=80&t=true'
+  },
+  {
+    name: 'PANAMERA',
+    brand: 'PORSHE',
+    period: 'Ao dia',
+    price: '325,00',
+    image_url: 'https://catalogo.webmotors.com.br/imagens/prod/347468/PORSCHE_PANAMERA_2.9_V6_EHYBRID_4_PDK_34746814472691347.png?s=fill&w=440&h=330&q=80&t=true'
+  }
+]
+
   return (
     <Container>
-      <Title>App List Service Home</Title>
+      <StatusBar barStyle="light-content" />
+      <Header>
+         <HeaderContent>
+          <Logo 
+              width={RFValue(108)}
+              height={RFValue(12)}  
+            />
+            <TotalCars>Total: {data.length} carros</TotalCars> 
+         </HeaderContent>
+      </Header>
+      
+      <ScrollView>
+        <TestCar>
+          {data.map((car) => (
+            <Car data={car} />
+          ))}
+        </TestCar>
+      </ScrollView>
+
     </Container>
   );
 }
--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,13 +1,33 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
-
-  background-color: ${({theme}) => theme.colors.background_primary}
+  background-color: ${({theme}) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-  font-family: ${({theme}) => theme.fonts.secondary_600};
+export const Header = styled.View`
+  font-family: ${({theme}) => theme.fonts.secondary_600}; 
+  width: 100%;
+  height: 113px;
+  background-color: ${({theme}) => theme.colors.header};
+  justify-content: flex-end;
+  padding: 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({theme}) => theme.fonts.primary_400};
+  color: ${({theme}) => theme.colors.text};
+`;
+
+export const TestCar = styled.View`
+  flex: 1;
+  padding: 15px;
 `;
